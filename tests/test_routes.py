6d73cdd256e1376f107ded1a9e55c04b895cfe34
romainrousselet <rrousselet2@gmail.com>
2024-12-26 11:43:45 -0500

    Commit message for the update and delete

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -144,3 +144,49 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        # send a self.client.post() request to the BASE_URL with a json payload of test_account.serialize()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        # assert that the resp.status_code is status.HTTP_201_CREATED
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # update the account
+        # get the data from resp.get_json() as new_account
+        new_account = resp.get_json()
+        # change new_account["name"] to something known
+        new_account["name"] = "Something Known"
+        # send a self.client.put() request to the BASE_URL with a json payload of new_account
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        # assert that the resp.status_code is status.HTTP_200_OK
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        # get the data from resp.get_json() as updated_account
+        updated_account = resp.get_json()
+        # assert that the updated_account["name"] is whatever you changed it to
+        self.assertEqual(updated_account["name"], "Something Known")
+
+    ######################################################################
+    # LIST ALL ACCOUNTS
+    ######################################################################
+'''
+    @app.route("/accounts", methods=["GET"])
+    def test_list_accounts():
+        """
+        List all Accounts
+        This endpoint will list all Accounts
+        """
+        app.logger.info("Request to list Accounts")
+        accounts = Account.all()
+        account_list = [account.serialize() for account in accounts]
+        app.logger.info("Returning [%s] accounts", len(account_list))
+        return jsonify(account_list), status.HTTP_200_OK
+'''
+def test_get_account_list(self):
+    """It should Get a list of Accounts"""
+    self._create_accounts(5)
+    resp = self.client.get(BASE_URL)
+    self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    data = resp.get_json()
+    self.assertEqual(len(data), 5)
